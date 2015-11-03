69647140f3335d9c7796ac66ed4ead7aa0cd1955
Ziyun Zhao <zhaoziy@hotmail.com>
2015-11-03 21:40:16 +0800

    十进制转八进制完成

--- Stack/Stack/Stack.cpp
+++ Stack/Stack/Stack.cpp
@@ -3,9 +3,42 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int TenToEight(int number)
+{
+	int eightNum = 0;
+	stack<int> eight;
+	while (number)
+	{
+		int mod = 0;
+		mod = number % 8;
+		eight.push(mod);
+		number = (number-mod) / 8;
+	}
+	while (1)
+	{
+		eightNum += eight.top();
+		eight.pop();
+		if (!eight.empty())
+		{
+			eightNum *= 10;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return eightNum;
+}
 
 int main()
 {
+	int a = 0;
+	a = TenToEight(1348);
     return 0;
 }
 
